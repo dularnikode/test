7107272943df742be6bee752b930b2ac43f3e25a
dularnikode <dularnikode@gmail.com>
2020-01-14 11:24:17 +0530

    Commiting add.cpp and it executable file test

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+int b;
+cout<<"SUM Program :"<<endl;
+cout<<"Enter the value for a: ";
+cin>>a;
+cout<<"Enter the value for b: ";
+cin>>b;
+printf("Addition of two no is :%d",a+b);
+return 0;
+}
